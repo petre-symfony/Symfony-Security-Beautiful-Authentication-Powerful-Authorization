f89810a487cf43a954c23bd74d0d75e8504bc36f
petrero <petrero@gmail.com>
2017-04-03 17:43:48 +0300

    Copy initial code from KNP site

---
+++ src/AppBundle/Controller/MainController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+  public function homepageAction(){
+    return $this->render('main/homepage.html.twig');
+  }
+}
