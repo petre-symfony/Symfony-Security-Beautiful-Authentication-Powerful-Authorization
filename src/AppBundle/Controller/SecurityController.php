6c535d7156ccb592170f1987ec9cc281af92402a
petrero <petrero@gmail.com>
2017-04-03 19:17:56 +0300

    Every login form looks about the same, so let's go steal some code. Google for 'Symfony security form login' and Find a page called How to Build a Traditional Login Form(http://symfony.com/doc/current/cookbook/security/form_login_setup.html).
    Adding the Login Controller
    
    Find their loginAction(), copy its code and paste it into ours.
    Notice, one thing is immediately weird: there's no form processing code inside of here. Welcome to the strangest part of Symfony's security. We will build the login form here, but some other magic layer will actually handle the form submit. We'll build that layer next.
    
    But thanks to this handy security.authentication_utils service, we can at least grab any authentication error that may have just happened in that magic layer as well as the last username that was typed in, which will actually be an email address for us.

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -11,7 +11,18 @@ class SecurityController extends Controller{
    * @Route("/login", name="security_login")
    */
   public function loginAction() {
-    
+    $authenticationUtils = $this->get('security.authentication_utils');
+
+    // get the login error if there is one
+    $error = $authenticationUtils->getLastAuthenticationError();
+
+    // last username entered by the user
+    $lastUsername = $authenticationUtils->getLastUsername();
+
+    return $this->render('security/login.html.twig', array(
+      'last_username' => $lastUsername,
+      'error'         => $error,
+    )); 
   }
 }
 
