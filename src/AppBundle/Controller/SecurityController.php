2c25efdd5115ec9af0a65943a077d7459ef7a12d
petrero <petrero@gmail.com>
2017-04-03 19:14:12 +0300

    Time to build a login form. And guess what? This page is no different than every other page: we'll create a route, a controller and render a template.
    For organization, create a new class called SecurityController. Extend the normal Symfony base Controller and add a public function loginAction().
    
    Setup the URL to be /login and call the route security_login.
    Make sure to auto-complete the @Route annotation so you get the use statement up top.

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class SecurityController extends Controller{
+  
+  /**
+   * @Route("/login", name="security_login")
+   */
+  public function loginAction() {
+    
+  }
+}
+
