caa74a2207bdc2366c6317f2ab42ec4d0855bd59
petrero <petrero@gmail.com>
2017-04-04 21:06:15 +0300

    I do use access controls to lock down big sections, but, mostly, I handle authorization inside my controllers.
    Deny Access (the long way)!
    
    Let's play around: comment out the access_control.
    
    And open up GenusAdminController. To check if the current user has a role, you'll always use one service: the authorization checker. It looks like this: if (!{dollar_sign}this->get('security.authorization_checker')->isGranted('ROLE_ADMIN'). So, if we do not have ROLE_ADMIN, then throw {dollar_sign}this->createAccessDeniedException():
    
    Head back and refresh. Access denied!
    
    So what's the magic behind that createAccessDeniedException() method? Find out: hold Command and click to open it. Ah, it literally just throws a special exception called AccessDeniedException:
    
     398 lines  vendor/symfony/symfony/src/Symfony/Bundle/FrameworkBundle/Controller/Controller.php
    ... lines 1 - 21
    use Symfony\Component\Security\Core\Exception\AccessDeniedException;
    ... lines 23 - 38
    abstract class Controller implements ContainerAwareInterface
    {
    ... lines 41 - 257
      /**
      * Returns an AccessDeniedException.
      *
      * This will result in a 403 response code. Usage example:
      *
      *     throw {dollar_sign}this->createAccessDeniedException('Unable to access this page!');
      *
      * @param string          {dollar_sign}message  A message
      * @param \Exception|null {dollar_sign}previous The previous exception
      *
      * @return AccessDeniedException
      */
      protected function createAccessDeniedException({dollar_sign}message = 'Access Denied.', \Exception {dollar_sign}previous = null)
      {
        return new AccessDeniedException({dollar_sign}message, {dollar_sign}previous);
      }
    ... lines 274 - 396
    }
    It turns out - no matter where you are - if you need to deny access for any reason, just throw this exception. Symfony handles everything else.

--- src/AppBundle/Controller/Admin/GenusAdminController.php
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -16,6 +16,9 @@ class GenusAdminController extends Controller {
    * @Route("/genus", name="admin_genus_list")
    */
   public function indexAction() {
+    if (!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')){
+      throw $this->createAccessDeniedException('GET OUT!');
+    }
     $genuses = $this->getDoctrine()
       ->getRepository('AppBundle:Genus')
       ->findAll();
