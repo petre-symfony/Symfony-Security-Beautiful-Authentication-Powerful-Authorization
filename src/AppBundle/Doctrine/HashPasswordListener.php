f4e2e9ebc8acfd86ea966840d0904304880281a5
petrero <petrero@gmail.com>
2017-04-04 18:45:59 +0300

    Next, add public function prePersist(). When Doctrine calls this, it will pass you an object called LifecycleEventArgs, from the ORM namespace.
    This method will be called before any entity is inserted. How do we know what entity is being saved? With {dollar_sign}entity = {dollar_sign}args->getEntity(). Now, if this is not an instanceof User, just return and do nothing

--- src/AppBundle/Doctrine/HashPasswordListener.php
+++ src/AppBundle/Doctrine/HashPasswordListener.php
@@ -3,9 +3,17 @@
 namespace AppBundle\Doctrine;
 
 use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LifecycleEventArgs;
 
 class HashPasswordListener implements EventSubscriber{
   public function getSubscribedEvents() {
     return ['prePersist', 'preUpdate'];  
-  }  
+  } 
+  
+  public function prePersist(LifecycleEventArgs $args){
+    $entity = $args->getEntity();
+    if (!$entity instanceof User){
+      return;
+    }
+  }
 }
