447f43c4ee418e4ce03418e41deae374d93a1c37
petrero <petrero@gmail.com>
2017-04-05 18:25:42 +0300

    That's it for security! We covered authentication and authorization. So, I'm not really sure why I'm still recording.
    Oh yea, I remember: let's create a registration form! Actually, this has nothing to do with security: registration is all about creating and saving a User entity. But, there are a few interesting things - call it a bonus round.
    
    Controller, Form, Check!
    
    Start like normal: create a new controller class called UserController - for stuff like registration and maybe future things like reset password.
    
    Inside, add registerAction() with the URL /register. Let's call the route user_register.
    
    Make sure you have your use statement for @Route.

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class UserController {
+  /**
+   * @ROUTE("/register", name="user_register")
+   */
+  public function registerAction(){
+    
+  }
+}
